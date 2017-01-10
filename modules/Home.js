38a22b6cc04e997e99034d5f5c67166515608fd1
adelbalso <a.delbalso@gmail.com>
2017-01-10 17:47:29 -0500

    link color

--- modules/Home.js
+++ modules/Home.js
@@ -7,7 +7,7 @@ export default React.createClass({
         <div className="profilePhoto" />
         <h1>Hello! I&#39;m Adrian Del Balso.</h1>
         <h1>I&#39;m a <span className="calloutText">digital product designer</span> from Montreal living in Brooklyn.</h1>
-        <h1>I am currently director of product at <span className="calloutText">the Foodworks.</span></h1>
+        <h1>I am currently director of product at <a href="http://www.thefoodworks.com">the Foodworks.</a></h1>
       </div>
     )
   }
